f8109d2a8cf4ab8a458d5d33ef83b39d59f5b03b
anner07 <torresmorel7@gmail.com>
2018-11-25 12:51:03 -0600

    agregando proveedores y compras

---
+++ rentas/win.rentas/FormClientes.cs
@@ -0,0 +1,154 @@
+﻿using BL.Rentas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormClientes : Form
+    {
+
+        ClientesBL _clientesBL;
+        CiudadesBL _ciudadesBL;
+
+        public FormClientes()
+        {
+            InitializeComponent();
+
+            _clientesBL = new ClientesBL();
+            listaClientesBindingSource.DataSource = _clientesBL.ObtenerClientes();
+
+            _ciudadesBL = new CiudadesBL();
+            listaCiudadesBindingSource.DataSource = _ciudadesBL.ObtenerCiudades();
+        }
+
+        private void listaClientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaClientesBindingSource.EndEdit();
+            var cliente = (Cliente)listaClientesBindingSource.Current;
+
+            if(fotoPictureBox.Image != null)
+            {
+                cliente.Foto = Program.imageToByteArray(fotoPictureBox.Image);
+            }
+            else
+            {
+                cliente.Foto = null;
+            }
+
+            var resultado = _clientesBL.GuardarCliente(cliente);
+
+            if (resultado.Exitoso == true)
+            {
+                listaClientesBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Cliente guardado");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (idTextBox.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+                }
+            }
+        }
+
+        private void Eliminar(int id)
+        {
+            var resultado = _clientesBL.EliminarCliente(id);
+
+            if (resultado == true)
+            {
+                listaClientesBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar el Cliente");
+            }
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            _clientesBL.CancelarCambios();
+            DeshabilitarHabilitarBotones(true);
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _clientesBL.AgregarCliente();
+            listaClientesBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void fotoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fotoPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cliente = (Cliente)listaClientesBindingSource.Current;
+
+            if (cliente != null)
+            {
+
+
+                openFileDialog1.ShowDialog();
+                var archivo = openFileDialog1.FileName;
+
+                if (archivo != "")
+                {
+                    var fileInfo = new FileInfo(archivo);
+                    var fileStream = fileInfo.OpenRead();
+
+                    fotoPictureBox.Image = Image.FromStream(fileStream);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Crear Cliente, antes de asignar imagen");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fotoPictureBox.Image = null;
+        }
+    }
+}
